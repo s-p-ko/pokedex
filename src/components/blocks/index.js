7cba9b7f674d5b4ae460675be6a8d9a1e4869a46
Serhii Kotsiuba <s-p-ko@ukr.net>
2022-02-14 14:23:51 +0200

    Update config, add background, components' folders

---
+++ src/components/blocks/index.js
@@ -0,0 +1,4 @@
+export { default as CardPokemon } from './CardPokemon';
+export { default as SearchPokemon } from './SearchPokemon';
+export { default as ModalDetailPokemon } from './ModalDetailPokemon';
+export { default as FilterPokemonsByType } from './FilterPokemonsByType';
---
+++ src/constants/config.js
@@ -0,0 +1,2 @@
+export const LIMIT_POKEMONS = 20;
+export const LIMIT_DELAY = 1000;
